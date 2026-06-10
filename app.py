4d6a36c8bf2eadd70e936bf246c4e2e19a4d0aff
LunaGTH <91calr21@gmail.com>
2026-06-10 13:25:59 -0600

    fix: workflow

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
-from calculator import add
+from calculator import add, subtract, multiply, divide
 
-print(add(5, 3))
+print("Prueba de calculadora")
+
+print("5 + 3 =", add(5, 3))
+print("10 - 4 =", subtract(10, 4))
+print("6 * 7 =", multiply(6, 7))
+print("20 / 5 =", divide(20, 5))
+
+print("Aplicación ejecutada correctamente")
